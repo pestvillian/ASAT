7c8109398f5e3cd37317dfae2aa782405d62e0f4
Greg Evan Ziegler <geziegle@ucsc.edu>
2025-03-07 14:20:34 -0800

    protocol Parsing Module Done

--- Greg/ESP32_experiments/SuccsessfulMotorTest/SuccsessfulMotorTest.ino
+++ Greg/ESP32_experiments/SuccsessfulMotorTest/SuccsessfulMotorTest.ino
@@ -1,8 +1,41 @@
+
+
 #define STEP_PIN 5
 #define DIR_PIN 6
+//psudo code for toggling direction
+  // Toggle direction every 1000 steps (adjust as needed)
+    // if (counter % 1000000 == 0) {  
+    //     DIR = (DIR == HIGH) ? LOW : HIGH;  // Toggle between HIGH and LOW
+    //     digitalWrite(DIR_PIN, DIR);  // Apply new direction
+    // }
+char protocols[] = {
+ "B9954","P9","M999959"
+};
+void parseProtocol(char protocols[]);
+
+void parseProtocol(char protocols[]){
+  serialPrint("helloWorld");
+}
 
 int DIR = HIGH;
 int counter = 0;
+//declare my runmotor function
+void runmotor(uint32_t,int,uint32_t);
+
+// operable frequency is about 400 to 1000
+void runmotor(uint32_t frequency,int direction,uint32_t steps){
+  uint32_t del = 1/frequency; //convert a frequency into a delay
+  digitalWrite(DIR_PIN,direction);
+//imitate the pwm library
+  for(int i = 0; i < steps; i++){ //this should pulse the motor for a desired distance
+    digitalWrite(STEP_PIN,HIGH); //high
+    delay(del);
+    digitalWrite(STEP_PIN,LOW); //low
+    delay(del);
+  }
+
+}
+
 
 void setup() {
     pinMode(STEP_PIN, OUTPUT);
@@ -11,17 +44,14 @@ void setup() {
 }
 
 void loop() {
-    counter++;
+  
+    // //this should run the motor in alternating directions continuously
+    // runmotor(600,HIGH,500);
+    // delay(50);
+    // runmotor(600,LOW,500);
+    // delay(50);
+    // // Step pulse
 
-    // Step pulse
-    digitalWrite(STEP_PIN, HIGH);
-    delayMicroseconds(71); // 7ms period
-    digitalWrite(STEP_PIN, LOW);
-    delayMicroseconds(71);
 
-    // Toggle direction every 1000 steps (adjust as needed)
-    if (counter % 1000 == 0) {  
-        DIR = (DIR == HIGH) ? LOW : HIGH;  // Toggle between HIGH and LOW
-        digitalWrite(DIR_PIN, DIR);  // Apply new direction
-    }
+    parseProtocols(Protocols);
 }
